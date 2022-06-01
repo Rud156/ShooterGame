b04d64d66b59540b3aeb0600fe8d302d6ae2dec5
Rud156 <rudrangshu_1997@yahoo.com>
2022-06-01 15:51:14 -0500

    More Anim Changes

---
+++ Assets/Scripts/Animatons/RandomAnimSelector.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Animations
+{
+    public class RandomAnimSelector : StateMachineBehaviour
+    {
+        [SerializeField] private int m_minAnimIndex;
+        [SerializeField] private int m_maxAnimIndex;
+        [SerializeField] private string m_animVariableName;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            int randomIndex = Random.Range(m_minAnimIndex, m_maxAnimIndex + 1);
+            animator.SetInteger(m_animVariableName, randomIndex);
+
+        }
+    }
+}
