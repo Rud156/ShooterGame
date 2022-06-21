a27749d9908616e7a001878b27412c397c3b0f0a
Rud156 <rudrangshu_1997@yahoo.com>
2022-06-21 00:06:50 -0500

    Updated Crosshair Controller

--- Assets/Scripts/UI/CrosshairController.cs
+++ Assets/Scripts/UI/CrosshairController.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,11 +12,15 @@ namespace UI
         [SerializeField][Range(0, 100)] private float m_crosshairLength;
         [SerializeField][Range(0, 1)] private float m_crosshairAlpha;
         [SerializeField] private Color m_crosshairColor;
+        [SerializeField][Range(0, 1)] private float m_crosshairOutlineThickness;
+        [SerializeField][Range(0, 1)] private float m_crosshairOutlineAlpha;
 
         [Header("Center Dot")]
         [SerializeField][Range(0, 100)] private float m_centerDotThickness;
         [SerializeField][Range(0, 1)] private float m_centerDotAlpha;
         [SerializeField] private Color m_centerDotColor;
+        [SerializeField][Range(0, 1)] private float m_centerDotOutlineThickness;
+        [SerializeField][Range(0, 1)] private float m_centerDotOutlineAlpha;
 
         [Header("Components")]
         [SerializeField] private CrosshairData m_crosshairLeft;
@@ -23,6 +28,11 @@ namespace UI
         [SerializeField] private CrosshairData m_crosshairTop;
         [SerializeField] private CrosshairData m_crosshairBottom;
         [SerializeField] private CrosshairData m_crosshairCenterDot;
+        [SerializeField] private CrosshairData m_crosshairLeftOutline;
+        [SerializeField] private CrosshairData m_crosshairRightOutline;
+        [SerializeField] private CrosshairData m_crosshairTopOutline;
+        [SerializeField] private CrosshairData m_crosshairBottomOutline;
+        [SerializeField] private CrosshairData m_crosshairCenterDotOutline;
 
         #region Unity Functions
 
@@ -39,18 +49,31 @@ namespace UI
 
         private void UpdateCrosshairTransform()
         {
-            m_crosshairLeft.transform.anchoredPosition = new Vector2(-m_crosshairOffset, 0);
-            m_crosshairRight.transform.anchoredPosition = new Vector2(m_crosshairOffset, 0);
-            m_crosshairTop.transform.anchoredPosition = new Vector2(0, m_crosshairOffset);
-            m_crosshairBottom.transform.anchoredPosition = new Vector2(0, -m_crosshairOffset);
+            m_crosshairLeftOutline.transform.anchoredPosition = new Vector2(-m_crosshairOffset, 0);
+            m_crosshairRightOutline.transform.anchoredPosition = new Vector2(m_crosshairOffset, 0);
+            m_crosshairTopOutline.transform.anchoredPosition = new Vector2(0, m_crosshairOffset);
+            m_crosshairBottomOutline.transform.anchoredPosition = new Vector2(0, -m_crosshairOffset);
 
-            m_crosshairLeft.transform.sizeDelta = new Vector2(m_crosshairLength, m_crosshairThickness);
-            m_crosshairRight.transform.sizeDelta = new Vector2(m_crosshairLength, m_crosshairThickness);
-            m_crosshairTop.transform.sizeDelta = new Vector2(m_crosshairThickness, m_crosshairLength);
-            m_crosshairBottom.transform.sizeDelta = new Vector2(m_crosshairThickness, m_crosshairLength);
+            m_crosshairLeftOutline.transform.sizeDelta = new Vector2(m_crosshairLength, m_crosshairThickness);
+            m_crosshairRightOutline.transform.sizeDelta = new Vector2(m_crosshairLength, m_crosshairThickness);
+            m_crosshairTopOutline.transform.sizeDelta = new Vector2(m_crosshairThickness, m_crosshairLength);
+            m_crosshairBottomOutline.transform.sizeDelta = new Vector2(m_crosshairThickness, m_crosshairLength);
+
+            float mappedLength = Mathf.Lerp(0, m_crosshairLength, 1 - m_crosshairOutlineThickness);
+            float mappedThickness = Mathf.Lerp(0, m_crosshairThickness, 1 - m_crosshairOutlineThickness);
+            m_crosshairLeft.transform.sizeDelta = new Vector2(mappedLength, mappedThickness);
+            m_crosshairRight.transform.sizeDelta = new Vector2(mappedLength, mappedThickness);
+            m_crosshairTop.transform.sizeDelta = new Vector2(mappedThickness, mappedLength);
+            m_crosshairBottom.transform.sizeDelta = new Vector2(mappedThickness, mappedLength);
         }
 
-        private void UpdateCenterDot() => m_crosshairCenterDot.transform.sizeDelta = new Vector2(m_centerDotThickness, m_centerDotThickness);
+        private void UpdateCenterDot()
+        {
+            m_crosshairCenterDotOutline.transform.sizeDelta = new Vector2(m_centerDotThickness, m_centerDotThickness);
+
+            float mappedThickness = Mathf.Lerp(0, m_centerDotThickness, 1 - m_centerDotOutlineThickness);
+            m_crosshairCenterDot.transform.sizeDelta = new Vector2(mappedThickness, mappedThickness);
+        }
 
         private void UpdateCrosshairColorAndAlpha()
         {
@@ -58,7 +81,13 @@ namespace UI
             m_crosshairRight.image.color = new Color(m_crosshairColor.r, m_crosshairColor.g, m_crosshairColor.b, m_crosshairAlpha); ;
             m_crosshairTop.image.color = new Color(m_crosshairColor.r, m_crosshairColor.g, m_crosshairColor.b, m_crosshairAlpha); ;
             m_crosshairBottom.image.color = new Color(m_crosshairColor.r, m_crosshairColor.g, m_crosshairColor.b, m_crosshairAlpha); ;
-            m_crosshairCenterDot.image.color = new Color(m_centerDotColor.r, m_centerDotColor.g, m_centerDotColor.b, m_centerDotAlpha); ;
+            m_crosshairCenterDot.image.color = new Color(m_centerDotColor.r, m_centerDotColor.g, m_centerDotColor.b, m_centerDotAlpha);
+
+            m_crosshairLeftOutline.image.color = new Color(0, 0, 0, m_crosshairOutlineAlpha);
+            m_crosshairRightOutline.image.color = new Color(0, 0, 0, m_crosshairOutlineAlpha);
+            m_crosshairTopOutline.image.color = new Color(0, 0, 0, m_crosshairOutlineAlpha);
+            m_crosshairBottomOutline.image.color = new Color(0, 0, 0, m_crosshairOutlineAlpha);
+            m_crosshairCenterDotOutline.image.color = new Color(0, 0, 0, m_centerDotOutlineAlpha);
         }
 
         #endregion Crosshair Setup
