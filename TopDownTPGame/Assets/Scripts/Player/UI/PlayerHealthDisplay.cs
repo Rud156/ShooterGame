f7076f4a70a33c3c9da7c3113cb359b8cb55cfaa
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2023-01-03 21:14:55 -0600

    Learning UI Toolkit

---
+++ TopDownTPGame/Assets/Scripts/Player/UI/PlayerHealthDisplay.cs
@@ -0,0 +1,44 @@
+#region
+
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+#endregion
+
+namespace UI
+{
+    public class PlayerHealthDisplay : MonoBehaviour
+    {
+        [Header("Debug")]
+        [SerializeField] [Range(0, 100)] private int _progressValue;
+
+        private VisualElement _root;
+        private VisualElement _playerHealth;
+
+        private ProgressBar _healthBar;
+
+        #region Unity Functions
+
+        private void Start()
+        {
+            _root = GetComponent<UIDocument>().rootVisualElement;
+            _playerHealth = _root.Q<VisualElement>("PlayerHealth");
+            _healthBar = (ProgressBar)_playerHealth;
+
+            var progressBar = _playerHealth.Q<VisualElement>("unity-progress-bar");
+            var progressBackground = progressBar.Q<VisualElement>(className: "unity-progress-bar__background");
+
+            var titleContainer = progressBackground.Q<VisualElement>(className: "unity-progress-bar__title-container");
+            var innerText = titleContainer.Q<Label>(className: "unity-progress-bar__title");
+            innerText.text = string.Empty;
+        }
+
+        private void Update()
+        {
+            _healthBar.value = _progressValue;
+        }
+
+        #endregion Unity Functions
+    }
+}
