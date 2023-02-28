f236761b7119a6fb017293569481c9c656556b5d
Rud156 <rudrangshu_1997@yahoo.com>
2023-02-28 08:57:42 -0600

    Added Proper Dash and Ultimate Animations

---
+++ TopDownTPGame/Assets/Scripts/AnimationControllers/ResetToDefaultOnState.cs
@@ -0,0 +1,16 @@
+﻿#region
+
+using UnityEngine;
+
+#endregion
+
+namespace Player.AnimationControllers
+{
+    public class ResetToDefaultOnState : StateMachineBehaviour
+    {
+        [SerializeField] private string _paramName;
+        [SerializeField] private int _defaultValue;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) => animator.SetInteger(_paramName, _defaultValue);
+    }
+}
