068847bbf24d7ce07d5046b427107af6e90f6078
Rud156 <rudrangshu_1997@yahoo.com>
2023-06-10 19:09:17 -0500

    Added self Owner Data for InstanceId

--- TopDownTPGame/Assets/Scripts/Utils/Common/OwnerData.cs
+++ TopDownTPGame/Assets/Scripts/Utils/Common/OwnerData.cs
@@ -9,13 +9,20 @@ namespace Utils.Common
         [SerializeField] private bool _autoSetOwnerId;
         [SerializeField] private GameObject _parent;
 
+        [Header("Self Data")]
+        [SerializeField] private bool _isSelfOwner;
+
         private int _ownerId;
 
         #region Unity Functions
 
         private void Start()
         {
-            if (_autoSetOwnerId)
+            if (_isSelfOwner)
+            {
+                _ownerId = gameObject.GetInstanceID();
+            }
+            else if (_autoSetOwnerId)
             {
                 _ownerId = _parent.GetInstanceID();
             }
