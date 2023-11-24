a9fa53dc7276c5db20475fffcd7211515602b9ae
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2023-11-24 11:01:36 -0600

    Adding Type 2 Character

---
+++ OneMoreShot/Assets/Scripts/Player/Type_2/Type_2_Primary_WaterStab.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Player.Type_2
+{
+    public class Type_2_Primary_WaterStab : MonoBehaviour
+    {
+    }
+}
---
+++ OneMoreShot/Assets/Scripts/Player/Type_2/Type_2_Secondary_WaterMovement.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Player.Type_2
+{
+    public class Type_2_Secondary_WaterMovement : MonoBehaviour
+    {
+    }
+}
